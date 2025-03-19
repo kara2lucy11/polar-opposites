65a04070f0e4a9084103a99009ae59b332b09e72
kara2lucy11 <kara2lucy11@gmail.com>
2025-03-19 18:21:22 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,67 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "=0S+x,!2`mk]#B(*5$vp":
+            case "ice":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 7 7 7 7 7 7 7 . . . 
+. . . . . 7 8 8 8 8 8 8 8 7 . . 
+. . . . 7 8 8 8 8 8 8 8 7 8 7 . 
+. . . . 7 7 7 7 7 7 7 7 8 8 7 . 
+. . . . 7 9 9 9 9 9 9 7 8 8 7 . 
+. . . . 7 9 7 9 9 7 9 7 8 8 7 . 
+. . . . 7 9 9 9 9 9 9 7 8 8 7 . 
+. . . . 7 9 9 9 9 9 9 7 8 8 7 . 
+. . . . 7 7 7 7 7 7 7 7 7 7 . . 
+. . . . . . . 7 7 7 . . . . . . 
+. . . . . 7 7 7 7 7 7 7 . . . . 
+. . . . . . . . 7 . . . . . . . 
+. . . . . . . . 7 . . . . . . . 
+. . . . . . . 7 7 7 . . . . . . 
+. . . . . . . 7 . 7 . . . . . . 
+`;
+            case "image1":
+            case "fire":return img`
+..........5.....
+.........55.....
+........5545....
+.......554555...
+......55425555..
+.....554222455..
+....55422222455.
+....5422222455..
+....544444455...
+....54444445....
+....52444425....
+....54444445....
+....54444445....
+....55555555....
+.......555......
+.....5555555....
+........5.......
+........5.......
+.......555......
+.......5.5......
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
